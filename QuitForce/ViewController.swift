b478723b746b2c87140eb3e3f8c5fe538a23e958
‘EugeneKirilov’ <evgeny.kirilov@innowise-group.com>
2022-12-29 13:34:58 +0300

    docs: add all files of the project

---
+++ QuitForce/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QuitForce
+//
+//  Created by Zenya Kirilov on 29.12.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
